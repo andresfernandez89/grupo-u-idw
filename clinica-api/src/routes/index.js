6b9c164e93aa50f2f2cdf89ea952ab5e25e55265
Andres Fernandez <andresfernandez.mdp@gmail.com>
2026-04-29 07:37:20 -0300

    chore: update import paths for route files to remove unnecessary suffixes

--- clinica-api/src/routes/index.js
+++ clinica-api/src/routes/index.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import authRoutes from './auth.routes.js'
-import especialidadesRoutes from './especialidades.routes.js'
-import estadisticasRoutes from './estadisticas.routes.js'
-import medicosRoutes from './medicos.routes.js'
-import obrasSocialesRoutes from './obras_sociales.routes.js'
-import pacientesRoutes from './pacientes.routes.js'
-import turnosRoutes from './turnos.routes.js'
+import authRoutes from './auth.js'
+import especialidadesRoutes from './especialidades.js'
+import estadisticasRoutes from './estadisticas.js'
+import medicosRoutes from './medicos.js'
+import obrasSocialesRoutes from './obras_sociales.js'
+import pacientesRoutes from './pacientes.js'
+import turnosRoutes from './turnos.js'
 
 const router = Router()
 
