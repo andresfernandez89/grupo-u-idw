2b9cf4318e032f8db5d8704229874b83a269156c
Cristopher Yang <dg.cris.yang@gmail.com>
2026-05-04 20:45:26 -0300

    feat: crear metodo update en controller de especialidades

--- clinica-api/src/controllers/especialidades.js
+++ clinica-api/src/controllers/especialidades.js
@@ -1,1 +1,14 @@
-export class EspecialidadesController {}
+import { EspecialidadesService } from "../services/especialidades.js";
+
+const service = new EspecialidadesService();
+
+export class EspecialidadesController {
+  async update(req, res) {
+    const id = parseInt(req.params.id);
+    const { nombre } = req.body;
+    const actualizada = await service.update(id, nombre);
+    if (!actualizada)
+      return res.status(404).json({ mensaje: "Especialidad no encontrada" });
+    res.json(actualizada);
+  }
+}
