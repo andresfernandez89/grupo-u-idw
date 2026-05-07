83cde2761b09e3d99c7759be2d94d96d74a78a96
Andres Fernandez <andresfernandez.mdp@gmail.com>
2026-05-07 10:08:49 -0300

    feat: implement EspecialidadesController with create method and add validation for Especialidad creation

--- clinica-api/src/controllers/especialidades.js
+++ clinica-api/src/controllers/especialidades.js
@@ -1,1 +1,33 @@
-export class EspecialidadesController {}
+import {
+  especialidadesCreate,
+  especialidadesResponse,
+} from "../dtos/especialidades.dto.js";
+import especialidadService from "../services/especialidades.js";
+
+export class EspecialidadesController {
+  async create(req, res) {
+    try {
+      const datos = especialidadesCreate(req.body);
+      const nuevaEspecialidad = await especialidadService.createEspecialidad(
+        datos.nombre,
+      );
+
+      return res.status(201).json({
+        success: true,
+        message: "Especialidad creada exitosamente",
+        data: especialidadesResponse(nuevaEspecialidad),
+      });
+    } catch (error) {
+      if (error.message.includes("ya está registrado")) {
+        return res.status(409).json({ success: false, message: error.message });
+      }
+
+      return res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+}
+
+export default new EspecialidadesController();
