6606ab60657b5b403691cf7d6bece16483ec24f8
Andres Fernandez <andresfernandez.mdp@gmail.com>
2026-05-07 10:06:26 -0300

    feat: implement createEspecialidad method in EspecialidadesService

--- clinica-api/src/services/especialidades.js
+++ clinica-api/src/services/especialidades.js
@@ -1,1 +1,13 @@
-export class EspecialidadesService {}
+import EspecialidadModel from "../models/especialidad.js";
+
+export class EspecialidadesService {
+  async createEspecialidad(nombre) {
+    const existing = await EspecialidadModel.findByNombre(nombre);
+    if (existing) {
+      throw new Error("El nombre de la especialidad ya está registrado");
+    }
+    return await EspecialidadModel.create(nombre);
+  }
+}
+
+export default new EspecialidadesService();
