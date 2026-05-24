e426c19936bb199039f2b6e4766a42ef792421ed
Gracimar <gracipm92@gmail.com>
2026-05-24 16:07:49 -0300

    feat: implementar rutas para pacientes

--- clinica-api/src/routes/v1/pacientes.js
+++ clinica-api/src/routes/v1/pacientes.js
@@ -1,5 +1,49 @@
 import { Router } from "express";
+import pacientesController from "../../controllers/pacientes.js";
+import { handleValidationErrors } from "../../middlewares/validacion.js";
+import {
+  browsePacienteValidator,
+  createPacienteValidator,
+  deletePacienteValidator,
+  getByIdPacienteValidator,
+  updatePacienteValidator,
+} from "../../validators/pacientes.js";
 
 const router = Router();
+
+router.get(
+  "/",
+  browsePacienteValidator,
+  handleValidationErrors,
+  pacientesController.browse.bind(pacientesController),
+);
+
+router.get(
+  "/:id",
+  getByIdPacienteValidator,
+  handleValidationErrors,
+  pacientesController.findById.bind(pacientesController),
+);
+
+router.post(
+  "/",
+  createPacienteValidator,
+  handleValidationErrors,
+  pacientesController.create.bind(pacientesController),
+);
+
+router.put(
+  "/:id",
+  updatePacienteValidator,
+  handleValidationErrors,
+  pacientesController.update.bind(pacientesController),
+);
+
+router.delete(
+  "/:id",
+  deletePacienteValidator,
+  handleValidationErrors,
+  pacientesController.delete.bind(pacientesController),
+);
 
 export default router;
