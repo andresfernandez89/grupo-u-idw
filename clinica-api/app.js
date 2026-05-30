3b6fcccbcfedeb6bb72012dc3c239fe44504db3b
Andres Fernandez <andresfernandez.mdp@gmail.com>
2026-05-29 22:04:31 -0300

    feat: refactor app structure by moving server logic to server.js and updating entry point in package.json

--- clinica-api/app.js
+++ clinica-api/app.js
@@ -1,32 +1,8 @@
 import cors from "cors";
 import express from "express";
-import { testConexion } from "./src/config/db.js";
 import v1Router from "./src/routes/v1/index.js";
 
-process.loadEnvFile();
-
-function validateEnv() {
-  const missing = [];
-
-  if (!process.env.DB_HOST) missing.push("DB_HOST");
-  if (!process.env.DB_USER) missing.push("DB_USER");
-  if (!process.env.DB_PASSWORD) missing.push("DB_PASSWORD");
-  if (!process.env.DB_NAME) missing.push("DB_NAME");
-  if (!process.env.CORS_ORIGIN) missing.push("CORS_ORIGIN");
-
-  if (missing.length > 0) {
-    throw new Error(
-      `Faltan variables de entorno obligatorias: ${missing.join(", ")}`,
-    );
-  }
-}
-
-validateEnv();
-
-await testConexion();
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Middlewares
 const corsOptions = {
@@ -42,9 +18,5 @@ app.use(express.json());
 // Routes
 app.use("/api", v1Router);
 app.use("/api/v1", v1Router);
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
 
 export default app;
---
+++ clinica-api/server.js
@@ -0,0 +1,30 @@
+import { testConexion } from "./src/config/db.js";
+import app from "./app.js";
+
+process.loadEnvFile();
+
+function validateEnv() {
+  const missing = [];
+
+  if (!process.env.DB_HOST) missing.push("DB_HOST");
+  if (!process.env.DB_USER) missing.push("DB_USER");
+  if (!process.env.DB_PASSWORD) missing.push("DB_PASSWORD");
+  if (!process.env.DB_NAME) missing.push("DB_NAME");
+  if (!process.env.CORS_ORIGIN) missing.push("CORS_ORIGIN");
+
+  if (missing.length > 0) {
+    throw new Error(
+      `Faltan variables de entorno obligatorias: ${missing.join(", ")}`,
+    );
+  }
+}
+
+validateEnv();
+
+await testConexion();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
